8c5828714662a64284b0e05e2099adbbe48e2bf0
aliyurekli32 <adamjesperblack@gmail.com>
2022-11-13 12:37:58 +0300

    news one

--- src/App.js
+++ src/App.js
@@ -27,13 +27,13 @@ function App() {
     
       <>
         <Routes>
-          <Route exact path="/" element ={<PrivateRouter isAuthorized={isAuthorized} />} >
+          <Route  path="/" element ={<PrivateRouter isAuthorized={isAuthorized} />} >
                 <Route path="" element ={<Home isAuthorized={isAuthorized} />}>
                     <Route path="about" element={<About isAuthorized={isAuthorized} />} />   
                     <Route index element={<Recipe flags={flags} setFlags={setFlags} isAuthorized={isAuthorized} />} />   
                 </Route>
           </Route>
-          <Route path="/login" element={<Login isAuthorized={isAuthorized} setIsAuthorized={setIsAuthorized} />} />
+          <Route exact path="/login" element={<Login isAuthorized={isAuthorized} setIsAuthorized={setIsAuthorized} />} />
           <Route path="/details" element={<Details isAuthorized={isAuthorized}  />} />  
         </Routes>
              
